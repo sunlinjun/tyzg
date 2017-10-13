e3ba20549e928747da3adc5df3be00c3507019a3
sunlinjun <sun_linjun@126.com>
2017-10-13 11:45:11 +0800

    gateway/src/main/java/com/gimis/GatewayApplication.java

--- gateway/src/main/java/com/gimis/GatewayApplication.java
+++ gateway/src/main/java/com/gimis/GatewayApplication.java
@@ -19,6 +19,8 @@ public class GatewayApplication {
 	
 	public static void main(String[] args) {
 		
+		System.out.println("gateway start ... ");
+		
 		SpringApplication.run(GatewayApplication.class, args);
 		
 		//配置文件
